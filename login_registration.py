634b8b1b5308fbbb68bb816eedb870aea10a7a8a
Elizaveta Syrkina <eliza1000@mail.ru>
2021-11-30 23:26:01 +0400

    Python files 3

--- login_registration.py
+++ login_registration.py
@@ -3,23 +3,23 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-#Регистрация
-# driver.get("http://practice.automationtesting.in/")
-#
-# account = driver.find_element_by_css_selector("#menu-item-50>a")
-# account.click()
-#
-# email = driver.find_element_by_id("reg_email")
-# email.send_keys("eliza1000@mail.ru")
-#
-# passw = driver.find_element_by_id("reg_password")
-# passw.send_keys("gfhjkmlkzfdnjvfnbpfwbb")
-#
-# reg = driver.find_element_by_name("register")
-# reg.click()
-# driver.quit()
+Регистрация
+driver.get("http://practice.automationtesting.in/")
 
-#Логин
+account = driver.find_element_by_css_selector("#menu-item-50>a")
+account.click()
+
+email = driver.find_element_by_id("reg_email")
+email.send_keys("eliza1000@mail.ru")
+
+passw = driver.find_element_by_id("reg_password")
+passw.send_keys("gfhjkmlkzfdnjvfnbpfwbb")
+
+reg = driver.find_element_by_name("register")
+reg.click()
+driver.quit()
+
+Логин
 import time
 from selenium import webdriver
 driver = webdriver.Chrome()
